c8f170c1448739b7c576aca5878ea909144d7617
GaboPP <gabriel.astorga@sansano.usm.cl>
2021-01-09 18:53:40 -0300

    acept change blovks

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,6 +7,8 @@
 #include <queue>
 #include <array>
 #include <algorithm>
+#include <sys/time.h>
+#include <iomanip>
 
 using namespace std;
 void lectura(vector<int> doctors_schedule_ref[5], int &M_maquinas_o, int &N_pacientes_o, int &D_doctores_o, int &P_r_o, int &P_p_o, int &P_u_o);
@@ -21,21 +23,20 @@ void clear(queue<int> &q );
 void Prettywrite(int** schedule_pac_doctor, int** schedule_pac_machine, int N_pacientes, int n_file);
 // Tabu search functions:
 bool check_tabu(queue<int> Tabu_list, int forbidden_i);
-int**  movement(int** S_c, int i, int U, int P, int R) ;
+void  movement(int** S_c_aux, int i, int U, int P, int R, int day, vector<int> aux) ;
 int f_ev(int** sol_d, int** sol_m, int N_pac);
 int best_N(int** local_best_d, int** local_best_m, int** S_c, int** S_m, queue<int> &Tabu_list, int U, int P, int R);
 void update_tabu_list (queue<int> &Tabu_list, int forbidden_movement, int max_size);
 void Tabu(int** solution_doctor, int **solution_machine, int max_size, int U, int P, int R, int stop_criterion);
 int** create2DArray(unsigned height, unsigned width);
 int** copy2DArray(int** array1, int** array2,int N, int M);
+void interchange(int** m, int first, int second, int from_row, int to_row);
 
 int main() {
     int M_maquinas=0; int N_pacientes=0; int D_doctores=0; 
     int P_r=0; int P_p=0; int P_u=0;
     vector<int> doctors_schedule_ref[5];
     lectura(doctors_schedule_ref, M_maquinas, N_pacientes, D_doctores, P_r, P_p, P_u);
-    cout << M_maquinas <<' '<<D_doctores <<' '<<N_pacientes << endl;
-    cout << P_r <<' '<<P_p <<' '<<P_u << endl;
     // cout << doctors_schedule_ref[0][0] << endl;
 
     int days = 20; // 4semanas X 5 días
@@ -53,11 +54,14 @@ int main() {
 
     PList(L_pacientes, P_u, P_p, P_r); // [N_sesions_p1, ...]
 
+    
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
     for(size_t day = 0; day < days; day++){
         if(accumulate(L_pacientes.begin(), L_pacientes.end(),0)==0){break;}
         paciente_queue(Q_paciente, L_pacientes, day, P_r,P_p,P_u);
         for (size_t bloque = 0; bloque < bloques; bloque++) {
-            // cout << " bloque "<< bloque << endl;
+            // cout << " bloque: "<< bloque << endl;
             maquina_queue(Q_Maq, M_maquinas);
             // cout << " en maquina " <<  Q_Maq.front();
             // cout << " con doctor " << Q_doctor.front() << endl;
@@ -84,10 +88,25 @@ int main() {
         }
     }
     
+    gettimeofday(&end, NULL);
+    double elapsed = ((end.tv_sec - start.tv_sec) * 1000) 
+           + (end.tv_usec / 1000 - start.tv_usec / 1000);
+    double test = 3/2;
+    cout << "tiempo greedy: ";
+    cout << setprecision(4) << test << endl; // Mostrara por pantalla 3.142
+    cout << setprecision(4) << elapsed << endl;
+
     Prettywrite(schedule_pac_doctor, schedule_pac_machine, N_pacientes, 1);
-    int max_size = 30;
+    int max_size = 7;
     int stop_criterion = 5000;
+    
+    gettimeofday(&start, NULL);
     Tabu(schedule_pac_doctor, schedule_pac_machine, max_size, P_u,P_p,P_r, stop_criterion);
+    gettimeofday(&end, NULL);
+    elapsed = ((end.tv_sec - start.tv_sec) * 1000) 
+           + (end.tv_usec / 1000 - start.tv_usec / 1000);
+    cout << "tiempo greedy: ";
+    cout << setprecision(4) << elapsed << endl; // Mostrara por pantalla 3.142
     Prettywrite(schedule_pac_doctor, schedule_pac_machine, N_pacientes, 2);
     return 0;
 }
@@ -184,7 +203,6 @@ void paciente_queue(queue<int> &Q_paciente, vector<int> &L_pacientes, int day,  
     }
 }
 
-
 void PList(vector<int> &L_pacientes, int P_u, int P_p, int P_r) {
     for (size_t i = 0; i < P_u+P_p+P_r; i++) {
         if(i < P_u){
@@ -196,7 +214,6 @@ void PList(vector<int> &L_pacientes, int P_u, int P_p, int P_r) {
         }
     }
 }
-
 void write(int schedule_pac_doctor[][20*16], int schedule_pac_machine[][20*16], int N_pacientes) {
     ofstream myfile;
     myfile.open("output");
@@ -274,49 +291,80 @@ void DList(vector<int> doctors_schedule[5], int Lun, int Ma, int Mie, int Jue, i
 
 
 bool check_tabu(queue<int> Tabu_list, int forbidden_i) {
+    // cout << "wa revisar mi señor, este wn de la cola me esta mirando-voy: " << Tabu_list.front() << " El largo de la cola es " << Tabu_list.size() << endl;
     for (size_t i = 0; i < Tabu_list.size(); i++) {   
+        // cout << "mirando al " << i << endl ;
         if(i==Tabu_list.front()) {
+            // cout << "encontre a este wn infraganti " << i << endl ;
             return true;
         }
         Tabu_list.pop();
     }
+    // cout << "Nadie mi señor" << endl;
     return false;
 }
-
-int**  movement(int **S_c, int i, int U, int P, int R) {
-    int** S_c_aux = create2DArray(U+P+R, 20*16);
-    int aux[20*16];
-    
-    S_c_aux = copy2DArray(S_c_aux, S_c, U+P+R, 20*16);
-    copy(&S_c_aux[i][0], &S_c_aux[i][0]+(20*16),&aux[0]);
-    if(i == U-1) {
-        copy(&S_c_aux[0][0], &S_c_aux[0][0]+(20*16),S_c_aux[i]); //  S_c_aux[i] = S_c_aux[0];
-        copy(&aux[0], &aux[0]+(20*16),S_c_aux[0]); //  S_c_aux[0] = aux;
-        return S_c_aux;
-        
-    } else if(i == U+P-1) {
-        copy(&S_c_aux[U][0], &S_c_aux[U][0]+(20*16),S_c_aux[i]);
-        copy(&aux[0], &aux[0]+(20*16),S_c_aux[U]);
-        return S_c_aux;
-    } else if(i == U+P+R-1) {
-        copy(&S_c_aux[P+U][0], &S_c_aux[P+U][0]+(20*16),S_c_aux[i]);
-        copy(&aux[0], &aux[0]+(20*16),S_c_aux[P+U]);
-        return S_c_aux;
-    } else {
-        copy(&S_c_aux[i][0], &S_c_aux[i][0]+(20*16),S_c_aux[i+1]);
-        copy(&aux[0], &aux[0]+(20*16),S_c_aux[i+1]);
-        return S_c_aux;
+void interchange(int** m, int first, int second, int from_row, int to_row) 
+{ 
+    for(int j = from_row; j < to_row; j++)
+    {
+        swap(m[first][j], m[second][j]);
     }
+}
+
+void  movement(int **S_c_aux, int paciente_i, int U, int P, int R, int day) {
+    // int** S_c_aux = create2DArray(U+P+R, 20*16);
+    // S_c_aux = copy2DArray(S_c_aux, S_c, U+P+R, 20*16);
+
+    // cout << paciente_i << " U: " << U << " P: " << P << " R: " << R << endl;
+    // copy(&S_c_aux[paciente_i][day*16], &S_c_aux[paciente_i][day*16]+(day*16 +16),&aux[0]);
+    // for (size_t i = 0; i < aux.size(); i++)
+    // {
+    //     cout << aux[i] << " ";
+    // }
+    // cout << endl;
+    if(paciente_i == U-1) {
+        // copy(&S_c_aux[0][day*16], &S_c_aux[0][day*16]+(day*16+ 16),S_c_aux[paciente_i]+(day*16)); //  S_c_aux[i] = S_c_aux[0];
+        // copy(&aux[0], &aux[0]+(16),S_c_aux[0]+(day*16)); //  S_c_aux[0] = aux;
+        interchange(S_c_aux, 0, paciente_i, day*16, day*16+16);
+    } 
+    else if(paciente_i == U+P-1) {
+        // copy(&S_c_aux[U][day*16], &S_c_aux[U][day*16]+(day*16 + 16), S_c_aux[paciente_i]+(day*16));
+        // copy(&aux[0], &aux[0]+(16), S_c_aux[U]+(day*16));
+        interchange(S_c_aux, U, paciente_i, day*16, day*16+16);
+    } 
+    else if(paciente_i == U+P+R-1) {
+        // copy(&S_c_aux[P+U][day*16], &S_c_aux[P+U][day*16]+(day*16+ 16),S_c_aux[paciente_i]+(day*16));
+        // copy(&aux[0], &aux[0]+(16),S_c_aux[P+U]+(day*16));
+        interchange(S_c_aux, P+U, paciente_i, day*16, day*16+16);
+    } 
+    else {
+        // if(day == 0){cout << "S_c_aux p0: "; for (size_t i = 0; i < 16; i++){ cout << " " <<  S_c_aux[paciente_i][day*16+ i] << " "; }; cout << endl;}
+        // if(day == 0){cout << "S_c_aux p1: "; for (size_t i = 0; i < 16; i++){ cout << " " <<  S_c_aux[paciente_i+1][day*16+ i] << " "; }; cout << endl;}
+         
+        // copy(&S_c_aux[paciente_i+1][day*16], &S_c_aux[paciente_i+1][day*16]+(day*16+ 16), S_c_aux[paciente_i]+(day*16));
+        // copy(&aux[0], &aux[0]+(16),S_c_aux[paciente_i+1]+(day*16));
+        interchange(S_c_aux, paciente_i, paciente_i+1, day*16, day*16+16);
+        // if(day == 0){cout << "S_c_aux p0: "; for (size_t i = 0; i < 16; i++){ cout << " " <<  S_c_aux[paciente_i][day*16+ i] << " "; }; cout << endl;}
+        // if(day == 0){cout << "S_c_aux p1: "; for (size_t i = 0; i < 16; i++){ cout << " " <<  S_c_aux[paciente_i+1][day*16+ i] << " "; }; cout << endl;}
+        
+    }
+    // delete[] aux;
+    return;
+
 }
 
 int f_ev(int** sol_d, int** sol_m, int N_pac) {
         int same_doctor = 0;
         int same_machine = 0;
         for (size_t index_pac = 0; index_pac < N_pac; index_pac++) { // sol_d
-            vector<int> doctores_pac;
-            vector<int> machines_pac;
-            for (size_t index_doc = 0; index_doc < N_pac; index_doc++) { // sol_d[index_pac]
-                if (index_doc != 0) {
+            // cout << "tamo activo con el pac" << index_pac << endl;
+            vector<int> doctores_pac; // same
+            vector<int> machines_pac; // REVISAR SI SE REINCIA
+            for (size_t index_doc = 0; index_doc < 20*16; index_doc++) { // sol_d[index_pac]
+                if (sol_d[index_pac][index_doc] != 0) {
+                    // if(index_pac>=14) {
+                    // cout << "bloque " << index_doc << " tamo activo con el pac "<< index_pac << " q tiene al doc" << sol_d[index_pac][index_doc]<< " con la machine " << sol_m[index_pac][index_doc] << endl;
+                    // }
                     if (binary_search(doctores_pac.begin(), doctores_pac.end(), sol_d[index_pac][index_doc])) { same_doctor = same_doctor +1;};
                     if (binary_search(machines_pac.begin(), machines_pac.end(), sol_m[index_pac][index_doc])) { same_machine = same_machine +1;};
                     doctores_pac.push_back(sol_d[index_pac][index_doc]);
@@ -325,25 +373,47 @@ int f_ev(int** sol_d, int** sol_m, int N_pac) {
                 }
             }
         }
-        return same_doctor+same_machine;
+        // cout << "valorado en: " << same_doctor+same_machine << endl;
+        return 0.7*same_doctor + 0.3*same_machine;
 }
 
 int best_N(int **local_best_d, int **local_best_m, int **S_c, int **S_m, queue<int> &Tabu_list, int U, int P, int R) {
-    int** neigbor_d = 0; neigbor_d = new int*[20*16];
-    int** neigbor_m = 0; neigbor_m = new int*[20*16];
-    // vector<int[][20*16]> neigborhood_m;
+    int** neigbor_d = create2DArray(U+P+R, 20*16);
+    int** neigbor_m = create2DArray(U+P+R, 20*16);
+    neigbor_d = copy2DArray(neigbor_d, S_c, U+P+R, 20*16);
+    neigbor_m = copy2DArray(neigbor_m, S_c, U+P+R, 20*16);
+
+    // vector<int> aux = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
     int forbidden_movement = 0;
-    for (size_t index = 0; index < (U+P+R); index++){
-        if (check_tabu(Tabu_list, index)) {continue;}
-        neigbor_d = movement(S_c, index, U,P,R);
-        neigbor_m = movement(S_c, index, U,P,R);
-        if (f_ev(neigbor_d, neigbor_m, U+P+R) > f_ev(local_best_d, local_best_m, U+P+R)){
-            for (size_t pac = 0; pac < (U+P+R); pac++) {
-                copy(&neigbor_d[pac][0], &neigbor_d[pac][0]+(20*16), local_best_d[pac]);
-                copy(&neigbor_m[pac][0], &neigbor_m[pac][0]+(20*16), local_best_m[pac]);
+    for (size_t day = 0; day < 20; day++) {
+        // cout << "day: " << day << endl;
+        for (size_t index = 0; index < (U+P+R); index++){
+            // for (size_t i = 0; i < 16; i++){cout << " " << aux[i] << " " ;/*aux[i] = 0;*/}
+            // cout << endl;
+            // cout << "Checking tabu for: " << index;
+            // cout << " El largo de la cola es " << Tabu_list.size() << endl;
+            if (Tabu_list.size()!= 0 && check_tabu(Tabu_list, index)) {
+                continue;
             }
-            forbidden_movement = index;
+            // cout << day << ".paciente: " << index << endl;
+            // cout << "wa a aser el dostor deseenme exito" << endl;
+            movement(neigbor_d, index, U,P,R, day);
+            // cout << "wa a aser la makina deseenme exito" << endl;
+            // for (size_t i = 0; i < 16; i++){cout << " " << aux[i] << " ";/*aux[i] = 0;*/}; cout << endl ;
+            movement(neigbor_m, index, U,P,R, day);
+            // cout << "Ojo ooojo. cheee viví pibee";
+            if (f_ev(neigbor_d, neigbor_m, U+P+R) > f_ev(local_best_d, local_best_m, U+P+R)){
+                cout << "mera wo ere mejol"<< endl;
+                for (size_t pac = 0; pac < (U+P+R); pac++) {
+                    copy(&neigbor_d[pac][0], &neigbor_d[pac][0]+(20*16), local_best_d[pac]);
+                    copy(&neigbor_m[pac][0], &neigbor_m[pac][0]+(20*16), local_best_m[pac]);
+                }
+                forbidden_movement = index;
+                // cout << "forbidden "<< forbidden_movement <<  endl;
+
+            }
         }
+        // cout << endl;
     }
     return forbidden_movement;
 }
@@ -364,10 +434,8 @@ void Tabu(int **solution_doctor, int **solution_machine, int max_size, int U, in
     int** S_best_d=create2DArray(U+P+R, 20*16); int **S_best_m=create2DArray(U+P+R, 20*16);
     int** S_v_d=create2DArray(U+P+R, 20*16); int **S_v_m=create2DArray(U+P+R, 20*16);
     int forbidden_movement;
-    cout << S_best_m[0][2] << endl;
-    cout << S_best_d[0][9] << endl;
     
-    cout << "best was found it! " << f_ev(solution_doctor, solution_machine, U+P+R) << endl;
+    // cout << "best was found it! " << f_ev(solution_doctor, solution_machine, U+P+R) << endl;
     for (size_t pac = 0; pac < U+P+R; pac++){
         copy(&solution_doctor[pac][0], &solution_doctor[pac][0]+(20*16), S_best_d[pac]);
         copy(&solution_machine[pac][0], &solution_machine[pac][0]+(20*16), S_best_m[pac]);
@@ -384,6 +452,7 @@ void Tabu(int **solution_doctor, int **solution_machine, int max_size, int U, in
                 copy(&solution_doctor[pac][0], &solution_doctor[pac][0]+(20*16), S_best_d[pac]);
                 copy(&solution_machine[pac][0], &solution_machine[pac][0]+(20*16), S_best_m[pac]);
             }
+            // cout << "EL PROHIBIDO ES " << forbidden_movement << endl;
             cout << "best foundit! " << f_ev(solution_doctor, solution_machine, U+P+R) << endl;
         }
         c +=1;
